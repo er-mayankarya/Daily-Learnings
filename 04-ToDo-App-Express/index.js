8d525a72b013f72ade4174610775815a8e283278
Mayank Arya <ermayankarya7@gmail.com>
2024-09-13 18:21:11 +0530

    Updated

---
+++ 04-ToDo-App-Express/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+
+const todos = [];
+
+// Add Todo
+app.post('/add' , (req, res) => {
+
+});
+
+// Update ToDO
+app.post('/update' , (req, res) => {
+
+});
+
+// Read ToDo
+app.get('/read' , (req, res) => {
+
+});
+
+// Delete ToDo
+app.delete('/delete' , (req , res) => {
+
+});
+
+app.listen(3000);
